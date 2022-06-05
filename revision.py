b726b4afcbef4bcdb3007fcfb4b6b40010d93969
vishnu.k <vishnu.k@yxiom.com>
2022-06-05 08:19:08 +0530

    Updated revision logic

--- revision.py
+++ revision.py
@@ -5,8 +5,7 @@ import sys
 from values import MAGOOSH_DICT
 
 while True:
-    known = []
-    unknown = []
+    count = 0
     choice = input("Choose one: [1: common_words | 2: basic | 3: advanced]")
     deck = MAGOOSH_DICT[int(choice)]
     y = input('Enter roman numeral for deck: ')
@@ -17,27 +16,24 @@ while True:
     print()
 
     print(len(words))
-    print("Press y and Enter if you know it or n if you don't know it: ")
+    print("Press Enter if you know it or n and Enter if you don't know it: ")
     i=0
     for k in words.keys():
         # print k
         i+=1
         val = input("\t"+str(i)+". "+k+": ")
         if val == "":
-            known.append(k)
+            count +=1
         else:
-            unknown.append(k)
             search_key('meaning', k,words)       
             search_key('mnemonic', k,words)
+            with open("unknowns_"+deck + y+".txt", 'w') as file:
+                    file.write(k+", ")
 
-    if len(known) == len(words):
+    if count == len(words):
         print("Congrats you finished whole set :)")
         if os.path.exists("unknowns_"+deck+y+".txt"):
             os.remove("unknowns_"+deck + y+".txt")
-    else:
-        with open("unknowns_"+deck + y+".txt", 'w') as file:
-            for word in unknown:
-                file.write(word+", ")
     ch = input("Press enter to try another set, CTRL+C to exit. ")
     print()
     print()
